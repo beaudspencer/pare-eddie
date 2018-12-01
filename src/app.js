27a6fd9bf8e63c5b67f09d4f99bc361447a3fcbb
beaudspencer <42357176+beaudspencer@users.noreply.github.com>
2018-11-30 21:12:03 -0800

    refactored app to use custom theme. #1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import {
-  withStyles
+  Button,
+  withStyles,
+  CssBaseline
 } from '@material-ui/core'
+import { 
+  createMuiTheme,
+} from '@material-ui/core/styles'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import grey from '@material-ui/core/colors/grey'
+import Navi from './navi'
 import Home from './home'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#212121'
+    }
+  },
+  background: {
+    default: '#E0E0E0'
+  },
+  typography: {
+    useNextVariants: true
+  }
+})
 
 export default function App(props) {
   return (
-    <Home />
+    <CssBaseline>
+      <MuiThemeProvider theme={theme}>
+        <React.Fragment>
+          <Navi />
+          <Home />
+        </React.Fragment>
+      </MuiThemeProvider>
+    </CssBaseline>
   )
 }

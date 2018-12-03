fa8161d762da03ed12bc96ac72a4fcbb68d6e050
beaudspencer <42357176+beaudspencer@users.noreply.github.com>
2018-12-03 13:28:29 -0800

    removed the maps react component and embeded a google maps api call return instead. #1

--- src/home.js
+++ src/home.js
@@ -7,18 +7,26 @@ import {
   Divider,
   withStyles
 } from '@material-ui/core'
+import GoogleMapReact from 'google-map-react'
 
 const styles = {
+  mapCont: {
+    margin: '0 auto',
+    marginTop: '1rem',
+    width: "90%",
+    height: "25vh",
+    frameBorder: "0"
+  },
   map: {
-    width: '80%',
-    height: '20rem'
+    height: '100%',
+    width: '100%'
   }
 }
 
 const InfoCard = withStyles({
   root: {
     position: 'relative',
-    width: '66%',
+    width: '88%',
     top: '3rem',
     margin: '0 auto',
     maxWidth: '40rem',
@@ -32,49 +40,64 @@ const WhiteDivider = withStyles({
   }
 })(Divider)
 
-export default function Home(props) {
-  return (
-    <div>
-    <InfoCard>
-      <CardContent>
-        <Typography
-          color="secondary"
-          gutterBottom
-        >
-          Location:
-        </Typography>
-        <Typography
-          color="secondary"
-        >
-          14181 Newport Ave STE E
-        </Typography>
-        <Typography
-          color="secondary"
-          gutterBottom
-        >
-          Tustin, CA 92780
-        </Typography>
-        <Typography
-          color='secondary'
-          gutterBottom
-        >
-          Phone:
-        </Typography>
-        <Typography
-          color="secondary"
-        >
-          (714) - 603 - 8753
-        </Typography>
-        <WhiteDivider
-          variant="fullWidth"
-        />
-        <div
-          style={style.map}
-          id="map"
-        >
-        </div>
-      </CardContent>
-    </InfoCard>
-    </div>
-  )
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.center = {
+      lat: 33.7349368,
+      lng: -117.8260378
+    }
+    this.zoom = 15
+  }
+  render() {
+    return (
+      <div>
+      <InfoCard>
+        <CardContent>
+          <Typography
+            color="secondary"
+            gutterBottom
+          >
+            Location:
+          </Typography>
+          <Typography
+            color="secondary"
+          >
+            14181 Newport Ave STE E
+          </Typography>
+          <Typography
+            color="secondary"
+            gutterBottom
+          >
+            Tustin, CA 92780
+          </Typography>
+          <Typography
+            color='secondary'
+            gutterBottom
+          >
+            Phone:
+          </Typography>
+          <Typography
+            color="secondary"
+          >
+            (714) - 603 - 8753
+          </Typography>
+          <WhiteDivider
+            variant="fullWidth"
+          />
+          <div
+          style={styles.mapCont}
+          >
+            <iframe
+              style={styles.map}
+              src="https://www.google.com/maps/embed/v1/place?q=14181%20Newport%20ave%20ste%20e&key=AIzaSyBsMgVI1Rrlw_Z2FnpCzme4epDsyyDNfHc"
+              allowFullScreen
+            >
+            </iframe>
+          </div>
+        </CardContent>
+      </InfoCard>
+      </div>
+    )
+  }
 }
